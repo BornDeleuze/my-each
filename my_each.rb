d392b511a13033be8c49245b21d6c335bcd36d84
Matt <ehxtwo@gmail.com>
2021-02-25 12:01:06 -0800

    commit tree be086de6194458825960ae36498185bb9f00f044

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,18 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  if block_given?
+    i = 0
+
+    while i < array.length
+      yield(array[i])
+      i += 1
+    end
+
+    array
+  else
+    return "Don't string me along now!"
+  end
+end 
+collection = [1, 2, 3, 4]
+my_each(collection) do |i|
+  return i
 end
